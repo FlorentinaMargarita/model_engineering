2312bd9e95e3755f71eb4cdd3292f42108060252
FlorentinaMargarita <florentina.hofbauer@gmail.com>
2023-02-22 19:36:22 -0800

    clean up

--- patterns/patterns.py
+++ patterns/patterns.py
@@ -1,6 +1,7 @@
 # patterns
 phone_number_pattern = r"\d{4,}"
 strange_signs_pattern = r"(\$[@$%&*!#?&ltgt]){2,}"
-all_caps_pattern = r"^[A-Z\s\W]+$"
+# two words in a row that have all upper case letters
+all_caps_pattern = r"\b[A-Z]+\s[A-Z]+\b"
 one_letter_words_in_sentence_other_than_i_id_a_u = r"\b(?!(i|I|u|U|a|A|i'd|I'd)\b)\w\b.*?\b(?!(i|I|u|U|a|A|i'd|I'd)\b)\w\b"
 same_letter_more_than_3x_pattern = r"\w*(\w{1})\1{2,}(?<!\.)\w*"
--- read_data.py
+++ read_data.py
@@ -9,35 +9,27 @@ import seaborn as sns
 
 mt.use('TkAgg')
 
-all_messages = []
 
-good_messages = []
+all_messages  = []
 
-with open('/mnt/c/Users/flore/Downloads/SMSSpamCollection.csv', 'r') as f:
+with open('./data/SMSSpamCollection.csv', 'r') as f:
     reader = csv.reader(f)
     
     for row in reader:
-        # getting rid of ham\t as part of cleaning the data 
-        # all_messages.append(row)
+        all_messages.append(row[0])
         
-        [all_messages.append(x) for x in row]
-
-        # all_messages.append(row)
-
-# print(all_messages)
-
 phone_number_spam_messages = []
 special_signs_spam_messages = []
 all_caps_spam_messages = []
 single_letter_word_messages = []
 same_letter_more_than_3x = []
 
-
+print(len(all_messages), 'trydata length')
 for message in all_messages: 
     phone_number = re.search(patterns.phone_number_pattern, message)
     
     if phone_number: 
-        phone_number_spam_messages.append(message)
+        phone_number_spam_messages.append(message[0])
 
     strange_pattern = re.search(patterns.strange_signs_pattern, message)
 
@@ -60,9 +52,6 @@ for message in all_messages:
     if same_letter_more_than_3_times_message: 
         same_letter_more_than_3x.append(message)
     
-    
-    
-
 
 all_messages__count = len(all_messages)
 same_letter_more_than_3x_count = len(same_letter_more_than_3x)
@@ -72,7 +61,6 @@ phone_number_spam_messages_count = len(phone_number_spam_messages)
 single_letter_word_messages_count = len(single_letter_word_messages)
 
 print(all_messages__count, 'all_messages__count')
-print(same_letter_more_than_3x_count, 'same_letter_more_than_3x_count')
 print(all_caps_spam_messages_count, 'all_caps_spam_messages_count')
 print(special_signs_spam_messages_count, 'special_signs_spam_messages_count')
 print(phone_number_spam_messages_count, 'phone_number_spam_messages_count')
@@ -107,27 +95,31 @@ labels =  ['not spam', 'spam']
 colors = {'not spam': 'blue', 'spam': 'red'}
 data = {'labels': labels, 'counts': counts}
 sns.set_style('whitegrid')
-sns.color_palette('pastel')
-sns.set_palette(sns.color_palette(colors.values()))
-ham_spam_data = pd.DataFrame(data)
-from matplotlib.patches import Patch
-legend_elements = [Patch(facecolor=color, label=label) for label, color in colors.items()]
-plt.legend(handles=legend_elements)
+# sns.color_palette('pastel')
+# sns.set_palette(sns.color_palette(colors.values()))
+# ham_spam_data = pd.DataFrame(data)
+# from matplotlib.patches import Patch
+# legend_elements = [Patch(facecolor=color, label=label) for label, color in colors.items()]
+# plt.legend(handles=legend_elements)
 
-df = pd.DataFrame(ham_spam_data)
+# df = pd.DataFrame(ham_spam_data)
 
-sns.set_style('darkgrid')
-plt.title('Spam vs Non-spam messages')
+# sns.set_style('darkgrid')
+# plt.title('Spam vs Non-spam messages')
+# print(df, 'df do')
+# print(ham_spam_data['counts'], 'ham_spam_data[counts]', ham_spam_data['labels'])
 
-plt.pie(ham_spam_data['counts'], labels=ham_spam_data['labels'], autopct='%1.1f%%')
+# plt.pie(ham_spam_data['counts'], labels=ham_spam_data['labels'], autopct='%1.1f%%')
 
-
-
-plt.show()
-
-# create a pie chart using seaborn
-plt.title('Spam messages')
-plt.pie(df['values'], labels=df['labels'], autopct='%1.1f%%', startangle=90)
 
 
 # plt.show()
+
+# create a pie chart using seaborn
+colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
+
+plt.title('Spam messages', fontweight='bold', bbox=dict(facecolor='red', edgecolor='black', boxstyle='round,pad=1'))
+plt.pie(showers['values'],  labels=showers['labels'], autopct='%1.1f%%', startangle=90, shadow=True,  textprops={'fontsize': 10})
+
+
+plt.show()
